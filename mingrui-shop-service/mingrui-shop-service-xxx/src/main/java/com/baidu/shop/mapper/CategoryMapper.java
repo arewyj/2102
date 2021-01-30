c2a58d4ddc00487724b0d0a586181c5e5ddfa35f
arewyj <wangyanjun_Jay@163.com>
2021-01-30 17:55:33 +0800

    ddd

---
+++ mingrui-shop-service/mingrui-shop-service-xxx/src/main/java/com/baidu/shop/mapper/CategoryMapper.java
@@ -0,0 +1,14 @@
+package com.baidu.shop.mapper;
+
+import com.baidu.shop.entity.CategoryEntity;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @ClassName CategoryMapper
+ * @Description: TODO
+ * @Author wyj
+ * @Date 2021/1/30
+ * @Version V1.0
+ **/
+public interface CategoryMapper extends Mapper<CategoryEntity> {
+}
